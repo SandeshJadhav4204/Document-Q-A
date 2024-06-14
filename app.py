8f5a9932617147042838291cf324630ca05fbd88
SandeshJadhav4204 <jadhav.sandesh4204@gmail.com>
2024-06-14 12:54:55 +0530

    Initial commit

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def index():
     return render_template('index.html', **session_state)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+
